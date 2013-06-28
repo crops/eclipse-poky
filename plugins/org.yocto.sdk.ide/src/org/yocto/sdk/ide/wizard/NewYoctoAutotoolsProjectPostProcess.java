1e5c3bfdfcda71e6ff1d2932b406a520353bc920
Jessica Zhang <jessica.zhang@intel.com>
2013-06-28 16:13:17 -0700

    Remote ADT changes by using URI based file access instead of IPath which only support local filesystem access
    Signed-off-by: Jessica Zhang <jessica.zhang@intel.com>

--- plugins/org.yocto.sdk.ide/src/org/yocto/sdk/ide/wizard/NewYoctoAutotoolsProjectPostProcess.java
+++ plugins/org.yocto.sdk.ide/src/org/yocto/sdk/ide/wizard/NewYoctoAutotoolsProjectPostProcess.java
@@ -14,7 +14,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URI;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.filesystem.IFileInfo;
 import org.eclipse.cdt.core.templateengine.TemplateCore;
 import org.eclipse.cdt.core.templateengine.process.ProcessArgument;
 import org.eclipse.cdt.core.templateengine.process.ProcessFailureException;
@@ -23,6 +27,7 @@ import org.eclipse.cdt.core.templateengine.process.processes.Messages;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.yocto.sdk.ide.YoctoSDKMessages;
 import org.yocto.sdk.ide.natures.YoctoSDKAutotoolsProjectNature;
@@ -47,6 +52,13 @@ public class NewYoctoAutotoolsProjectPostProcess extends ProcessRunner {
 						YoctoSDKMessages.getFormattedString("AutotoolsProjectPostProcess.WrongProjectNature", //$NON-NLS-1$
 								projectName));
 			} else {
+				URI path = project.getLocationURI();
+				IFileStore fs = EFS.getStore(path);
+				fs = fs.getFileStore(new Path(AUTOGEN_SCRIPT_NAME));
+				IFileInfo fileinfo = EFS.createFileInfo();
+				fileinfo.setAttribute(EFS.ATTRIBUTE_EXECUTABLE, true);
+				fs.putInfo(fileinfo,EFS.SET_ATTRIBUTES, null);
+				/*
 				IPath path = project.getLocation();
 				String path_str = path.toString();
 				String autogen_cmd = CHMOD_COMMAND + path_str + File.separator + AUTOGEN_SCRIPT_NAME;
@@ -72,7 +84,7 @@ public class NewYoctoAutotoolsProjectPostProcess extends ProcessRunner {
 				} catch (Throwable t) {
 					t.printStackTrace();
 
-				}
+				}*/
 			}
 		} catch (Exception e) {
 			throw new ProcessFailureException(Messages.getString("NewManagedProject.3") + e.getMessage(), e); //$NON-NLS-1$
