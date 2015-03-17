76d200429784fc7f930fa556b3530d3c006d40f3
Todor Minchev <todor.minchev@intel.com>
2015-03-17 10:19:23 +0000

    scripts/generate-doc.sh: download documentation over http
    Signed-off-by: Todor Minchev <todor.minchev@intel.com>

--- scripts/generate-doc.sh
+++ scripts/generate-doc.sh
@@ -56,7 +56,7 @@ DOC_PLUGIN_DIR=${PLUGIN_FOLDER}/plugins/org.yocto.doc.user
 DOC_HTML_DIR=${DOC_PLUGIN_DIR}/html/
 
 # git clone
-DOC_GIT=git://git.yoctoproject.org/yocto-docs.git
+DOC_GIT=http://git.yoctoproject.org/git/yocto-docs
 git clone ${DOC_GIT} ${DOC_DIR} || fail $? "git clone ${DOC_GIT}"
 cd ${DOC_DIR}
 git checkout ${REFERENCE} || fail $? "git checkout ${REFERENCE}"
