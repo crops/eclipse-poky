f5e33fc79a3f77b0013c005019b72430d8998e8b
Todor Minchev <todor.minchev@intel.com>
2015-03-27 15:36:48 +0000

    scripts/generate-doc.sh: always build documentation
    Signed-off-by: Todor Minchev <todor.minchev@intel.com>

--- scripts/generate-doc.sh
+++ scripts/generate-doc.sh
@@ -3,12 +3,10 @@
 help()
 {
 	echo "Generate and add eclipse help from yocto's documentation"
-	echo "Usage: $0 BRANCH_NAME PLUGIN_FOLDER"
-	echo "       $0 -t TAG_NAME PLUGIN_FOLDER"
-	echo ""
+	echo -e "Usage: $0 BRANCH_NAME | TAG_NAME PLUGIN_FOLDER\n"
 	echo "Options:"
 	echo "-h - display this help and exit"
-	echo "-t TAG_NAME - tag to build the documentation upon"
+	echo "TAG_NAME - tag to build the documentation upon"
 	echo "BRANCH_NAME - branch to build the documentation upon"
 	echo "PLUGIN_FOLDER - root folder of the eclipse-poky project"
 	exit 1
@@ -25,13 +23,10 @@ fail ()
 }
 
 CHECKOUT_TAG=0
-while getopts ":ht" opt; do
+while getopts ":h" opt; do
 	case $opt in
 		h)
 			help
-			;;
-		t)
-			CHECKOUT_TAG=1
 			;;
 	esac
 done
@@ -41,11 +36,6 @@ if [ $# -ne 2 ]; then
 	help
 fi
 
-if [ $CHECKOUT_TAG -eq 0 ]; then
-	REFERENCE=origin/$1
-else
-	REFERENCE=$1
-fi
 PLUGIN_FOLDER=`readlink -f $2`
 
 TOP=`pwd`
@@ -59,7 +49,8 @@ DOC_HTML_DIR=${DOC_PLUGIN_DIR}/html/
 DOC_GIT=http://git.yoctoproject.org/git/yocto-docs
 git clone ${DOC_GIT} ${DOC_DIR} || fail $? "git clone ${DOC_GIT}"
 cd ${DOC_DIR}
-git checkout ${REFERENCE} || fail $? "git checkout ${REFERENCE}"
+
+git checkout $1 || fail $? "git checkout $1"
 COMMIT_ID=`git rev-parse HEAD`
 
 # build and copy
@@ -67,14 +58,6 @@ DOCS="yocto-project-qs adt-manual kernel-dev \
       bsp-guide ref-manual dev-manual profile-manual"
 
 cd documentation
-ECLIPSE_TARGET_AVAILABLE=`make -q eclipse &> /dev/null; echo $?`
-if [ ${ECLIPSE_TARGET_AVAILABLE} -ne 1 ]; then
-	echo "WARNING:"
-	echo "This version does not support generating eclipse help"
-	echo "Documentation will not be available in eclipse"
-	exit 1
-fi
-
 for DOC in ${DOCS}; do
 	make DOC=${DOC} eclipse
 	cp -rf ${DOC}/eclipse/html/* ${DOC_HTML_DIR}
