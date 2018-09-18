847792e5a0c03dd59966a0a7be3b92fff64b5e7c
Chin Huat Ang <chin.huat.ang@intel.com>
2018-09-18 13:33:46 +0800

    org.yocto.sdk.autotools.ui: configure as Yocto Project autotools project
    Add a configure as Yocto Project autotools project right click menu
    option, which adds Yocto Project nature and sets the Yocto Project
    autotools toolchain to the selected project.
    
    Signed-off-by: Chin Huat Ang <chin.huat.ang@intel.com>

---
+++ plugins/org.yocto.sdk.autotools.ui/src/org/yocto/sdk/autotools/ui/ConfigureYoctoProjectAutotoolsProjectAction.java
@@ -0,0 +1,11 @@
+package org.yocto.sdk.autotools.ui;
+
+import org.yocto.sdk.ui.actions.EnableNatureAction;
+
+public class ConfigureYoctoProjectAutotoolsProjectAction extends EnableNatureAction {
+
+	public ConfigureYoctoProjectAutotoolsProjectAction() {
+		super("org.yocto.sdk.autotools.core.toolChain.debug"); //$NON-NLS-1$
+	}
+
+}
