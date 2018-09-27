2c57146b07a5e29dd63aa8e89572aec887b49592
Chin Huat Ang <chin.huat.ang@intel.com>
2018-09-27 10:36:47 +0800

    org.yocto.sdk.autotools.ui: fix autotools toolchain settings
    Make sure Autotools project converted to use Yocto Project Autotools
    toolchain is configured with a new configuration with the expected
    toolchain ID. Also make sure the configure tool is executed with
    ${CONFIGURE_FLAGS} variable set by the environment setup script.
    
    Signed-off-by: Chin Huat Ang <chin.huat.ang@intel.com>

--- plugins/org.yocto.sdk.autotools.ui/src/org/yocto/sdk/autotools/ui/ConfigureYoctoProjectAutotoolsProjectAction.java
+++ plugins/org.yocto.sdk.autotools.ui/src/org/yocto/sdk/autotools/ui/ConfigureYoctoProjectAutotoolsProjectAction.java
@@ -1,11 +1,33 @@
 package org.yocto.sdk.autotools.ui;
 
+import org.eclipse.cdt.internal.autotools.core.configure.AutotoolsConfigurationManager;
+import org.eclipse.cdt.internal.autotools.core.configure.IAConfiguration;
+import org.eclipse.cdt.managedbuilder.core.IManagedBuildInfo;
+import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.yocto.sdk.ui.actions.EnableNatureAction;
 
+@SuppressWarnings("restriction")
 public class ConfigureYoctoProjectAutotoolsProjectAction extends EnableNatureAction {
 
 	public ConfigureYoctoProjectAutotoolsProjectAction() {
-		super("org.yocto.sdk.autotools.core.toolChain.debug"); //$NON-NLS-1$
+		super("org.yocto.sdk.autotools.core.toolChain"); //$NON-NLS-1$
+	}
+
+	@Override
+	protected void updateCProjectDescription(IProject project) throws CoreException {
+
+		super.updateCProjectDescription(project);
+
+		IManagedBuildInfo info = ManagedBuildManager.getBuildInfo(project);
+		IAConfiguration aCfg = AutotoolsConfigurationManager.getInstance().getConfiguration(project,
+				info.getDefaultConfiguration().getId());
+		// Make sure configure picks up --host, --build, --target etc.
+		aCfg.setOption("configure", "configure ${CONFIGURE_FLAGS}"); //$NON-NLS-1$ //$NON-NLS-2$
+		AutotoolsConfigurationManager.getInstance().addConfiguration(project, aCfg);
+		AutotoolsConfigurationManager.getInstance().saveConfigs(project);
+
 	}
 
 }
