f64e2007ecc893243e011f3d4b1bba3b2ceda428
Chin Huat Ang <chin.huat.ang@intel.com>
2019-01-11 06:58:26 +0800

    setup.sh: update Eclipse Oxygen SDK upstream url
    Eclipse Oxygen 4.7.3a has been moved to archive.eclipse.org, running
    setup.sh with --upstream switch should now download from the new URL.
    
    [YOCTO #13101]
    
    Signed-off-by: Chin Huat Ang <chin.huat.ang@intel.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -116,7 +116,7 @@ if [ ! -f eclipse/plugins/org.eclipse.swt_3.106.3.v20180329-0507.jar ]; then
 
   if [[ "$1" = "--upstream" ]]
   then
-        wget "http://download.eclipse.org/eclipse/downloads/drops4/${ep_rel}${ep_ver}${ep_date}/eclipse-SDK-${ep_ver}-${ep_arch}.tar.gz"
+        wget "http://archive.eclipse.org/eclipse/downloads/drops4/${ep_rel}${ep_ver}${ep_date}/eclipse-SDK-${ep_ver}-${ep_arch}.tar.gz"
   else
         wget "http://downloads.yoctoproject.org/eclipse-full/eclipse/downloads/drops4/${ep_rel}${ep_ver}${ep_date}/eclipse-SDK-${ep_ver}-${ep_arch}.tar.gz"
   fi
